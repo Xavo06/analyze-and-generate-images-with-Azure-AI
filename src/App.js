5d2284cfbb168263f8670a86c89aef65a50958d7
Xavo06 <150401624+Xavo06@users.noreply.github.com>
2023-11-20 19:08:03 +0000

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import React from 'react';
 
-function App() {
-  const value = 'World';
-  return <div>Hullo {value}</div>;
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>OpenSSL Image Analyzer</title>
+</head>
+<body>
+  <h1>OpenSSL Image Analyzer</h1>
 
-export default App;
+  <label for="imageUrl">Dirección URL de la imagen:</label>
+  <input type="text" id="imageUrl" placeholder="Ingrese la URL de la imagen">
+
+  <button onclick="analizarImagen()">Analizar Imagen</button>
+  <button onclick="generarImagen()">Generar Imagen</button>
+
+  <script>
+    function analizarImagen() {
+      // Lógica para analizar la imagen
+      var imageUrl = document.getElementById("imageUrl").value;
+      // Aquí puedes realizar acciones con la URL de la imagen para análisis
+      console.log("Analizando la imagen: " + imageUrl);
+    }
+
+    function generarImagen() {
+      // Lógica para generar la imagen
+      var imageUrl = document.getElementById("imageUrl").value;
+      // Aquí puedes realizar acciones con la URL de la imagen para generación
+      console.log("Generando la imagen: " + imageUrl);
+    }
+  </script>
+</body>
+</html>
