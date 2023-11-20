2e65333b6f444a00cdf529d25d4d3e0fd8a8a4a2
Xavo06 <150401624+Xavo06@users.noreply.github.com>
2023-11-20 22:03:30 +0000

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
